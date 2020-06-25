9ac8ffc5fb6c5a7ada2d87274c60de8cb3d94fe5
Warren Harper II <warrenharper2@att.net>
2020-06-25 21:19:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,11 +23,11 @@ let allWagesFor = function () {
 
 function createEmployeeRecord(employee) {
     let emp = {
-        firstName: employee[0], 
-        familyName: employee[1], 
-        title: employee[2], 
-        payPerHour: employee[3], 
-        timeInEvents: [], 
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
         timeOutEvents: []
     }
     return emp
@@ -40,8 +40,8 @@ function createEmployeeRecords(array) {
 
 function createTimeInEvent(date) {
    let event = {
-       type: "TimeIn", 
-       hour: parseInt(date.split(" ")[1], 10), 
+       type: "TimeIn",
+       hour: parseInt(date.split(" ")[1], 10),
        date: date.split(" ")[0]
    }
     this.timeInEvents.push(event)
@@ -49,10 +49,10 @@ function createTimeInEvent(date) {
 }
 
 function hoursWorkedOnDate(date) {
-    
+
     let timeIn = this.timeInEvents.filter(d => d.date === date)[0]
     let timeOut = this.timeOutEvents.filter(d => d.date===date)[0]
-    
+
 
     return (timeOut.hour - timeIn.hour)/100
 }
@@ -60,24 +60,24 @@ function hoursWorkedOnDate(date) {
 function wagesEarnedOnDate(date) {
     // let pay =  this.hoursWorkedOnDate(date).reduce((h, total) => total += h) * this.payPerHour
     return hoursWorkedOnDate.call(this, date) * this.payPerHour
-    
+
 }
 
 function createTimeOutEvent(date) {
 
     let event = {
-        type: "TimeOut", 
-        hour: parseInt(date.split(" ")[1], 10), 
+        type: "TimeOut",
+        hour: parseInt(date.split(" ")[1], 10),
         date: date.split(" ")[0]
     }
      this.timeOutEvents.push(event)
      return this
 }
 function findEmployeeByFirstName(collection, firstNameString) {
-    
-    
+
+
     let employee = collection.filter(d => d.firstName === firstNameString)[0]
-    
+
     return employee
 
 }
